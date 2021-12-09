926bd926a1811f8e4590247ac4247333203d9cee
fathibensari <fethibensari@gmail.com>
2021-12-09 15:07:36 +0100

    add liveHost url value

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,4 @@
 const liveHost = "https://us-central1-mealstogo-78c2c.cloudfunctions.net"
 const localHost = "http://192.168.1.6:5001/mealstogo-78c2c/us-central1"
 const isDev = process.env.NODE_ENV === "development"
-export const host = isDev ? localHost : liveHost
+export const host = false ? localHost : liveHost
